0e24b994b855c9bbc0d990795fd0dcc5364f1246
kerim_tarik <kimot@DESKTOP-6N9AOMP>
2022-12-10 00:23:39 +0300

    registration page started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react"
+import Nav from './Components/Nav'
+import Body from "./Components/RegistrationBody"
+
+export default function App(){
+  return(
+<>
+<Nav/>
+<Body/>
+</>
+  )
+}
