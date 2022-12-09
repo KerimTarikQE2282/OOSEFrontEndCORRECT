e58cf172e79e28797b4ca7744c8e1c51a0f13f62
kerim_tarik <kimot@DESKTOP-6N9AOMP>
2022-12-09 23:58:15 +0300

    registration page started

---
+++ oosefrontend/src/Components/RegistrationBody.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Side from "./RegistrationSIde"
+import Form from "./FORM"
+
+
+export default function Body(){
+
+    return(
+        <div className="Body">
+            <Side/>
+            <Form/>
+        </div>
+    )
+}
+
